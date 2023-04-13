906197683dfdf89d1a6439bf9078ef389b4ca1a3
Gleb Potapov <122749679+PotapovGN@users.noreply.github.com>
2023-04-13 21:28:16 +0300

    Create solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 485082255 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    control_cr = x_success / x_cnt
+    test_cr = y_success / y_cnt
+    pooled_cr = (x_success + y_success) / (x_cnt + y_cnt)
+    pooled_se = (pooled_cr * (1 - pooled_cr) * (1 / x_cnt + 1 / y_cnt)) ** 0.5
+    z_score = (test_cr - control_cr) / pooled_se
+    p_value = stats.norm.sf(z_score)
+    
+    return p_value < alpha   
